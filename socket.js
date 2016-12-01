5ffdb96a7085d7c38b835c313640a81921a89946
Nicolas MAZARD <nicolas.mazard@gmail.com>
2016-12-01 23:12:48 +0100

    first message by servor

--- socket.js
+++ socket.js
@@ -7,7 +7,18 @@ function init(http){
 }
 
 function onConnection(socket){
+	var messages = [];
+	var name = null;
 	console.info("Socket connection on id "+socket.id)
+	socket.on('request-access', (obj) =>{
+		name = obj.name
+		socket.emit('access-granted')
+		socket.emit('send_message', {
+			message : 'COUCOU !!!!!!! C\'EST MOI ALFRED !!!!',
+			time : new Date(),
+			author : 'Alfred'
+		})
+	})
 }
 
 exports.init = init;
