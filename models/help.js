385f73dd37e1b4efab42d3eedfb03a6e54c87680
Nicolas MAZARD <nicolas.mazard@gmail.com>
2016-12-02 01:52:32 +0100

    add Help

--- models/help.js
+++ models/help.js
@@ -1,10 +1,9 @@
 const Message = require('../Message')
 
 exports.regex = [
-	/bonjour/i,
-	/salut/i
+	/help/
 ]
 
 exports.perform = function(message,callback){
-	callback(new Message("Bonjour agent "+message.author,"Alfred"))	
+	callback(new Message("","Alfred"))	
 }
