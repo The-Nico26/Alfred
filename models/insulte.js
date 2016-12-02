cf2e6c1c9c3c554af4c9ba6a031fdd69420b1310
GiSnafu <alexisrangheard@outlook.fr>
2016-12-02 05:25:05 +0100

    insultes

---
+++ models/insulte.js
@@ -0,0 +1,24 @@
+var insulte = [
+	"C'est n'est pas correct ",
+	"Surveille ton langage gamin ",
+	"Ta mère ne serait pas fier ",
+	"Les insultes est le langages des pauvres"
+]
+
+exports.regex = [
+	/connard/i,
+	/putain/i,
+	/ferme-la( !)?/i,
+	/pute/i,
+	/nique/i,
+	/gueule/i,
+	/foutre/i,
+	/suce/i,
+	/merde/i,
+
+]
+
+exports.perform = function(message,socket){
+	var insulte = insultes[Math.round(Math.random()*(insultes.length-1))]
+	socket.sendMessage(insulte)	
+}
